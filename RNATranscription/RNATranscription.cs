c8e62b2a3c8736e5f1bd826a551558de3b1e036d
VaishnaviSahu <vaishnavi.sahu@cgi.com>
2018-09-11 14:45:46 +0530

    .net3

--- RNATranscription/RNATranscription.cs
+++ RNATranscription/RNATranscription.cs
@@ -5,8 +5,28 @@ namespace RNATranscriptionModule
     public static class RnaTranscription
     {
         public static string ToRna(string nucleotide)
-        {
-            throw new NotImplementedException("You need to implement this function.");
+        {  
+            String rna="";
+            for(int i=0; i<nucleotide.Length; i++)
+            {
+                if(nucleotide[i]=='G')
+                {
+                    rna=rna+"C";
+                }
+              else if(nucleotide[i]=='C')
+                {
+                     rna=rna+"G";
+                }
+               else if(nucleotide[i]=='T')
+                {
+                     rna=rna+"A";
+                }
+               else if(nucleotide[i]=='A')
+                {
+                    rna=rna+"U";
+                }
+            }
+            return (rna);
         }
     }
 }
